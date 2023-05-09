baa80f5220b17a21086bff808110b970807dde0c
ParvinValizade <parvin.valizade@mail.ru>
2023-05-09 22:51:53 +0400

    railway

---
+++ src/main/java/com/company/metaverse/config/CorsFilter.java
@@ -0,0 +1,27 @@
+package com.company.metaverse.config;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CorsFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        HttpServletResponse httpServletResponse = (HttpServletResponse) response;
+        httpServletResponse.setHeader("Access-Control-Allow-Origin", "*");
+        httpServletResponse.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+        httpServletResponse.setHeader("Access-Control-Max-Age", "3600");
+        httpServletResponse.setHeader("Access-Control-Allow-Headers", "authorization, content-type, xsrf-token");
+        httpServletResponse.addHeader("Access-Control-Expose-Headers", "xsrf-token");
+        httpServletResponse.addHeader("Access-Control-Expose-Headers", "http://localhost:3000");
+
+        chain.doFilter(request, response);
+    }
+}
