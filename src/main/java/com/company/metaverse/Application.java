eabcb04161349b2cfc87e99e3b22514d40882e0d
ParvinValizade <parvin.valizade@mail.ru>
2023-05-10 18:19:37 +0400

    railway

--- src/main/java/com/company/metaverse/Application.java
+++ src/main/java/com/company/metaverse/Application.java
@@ -2,11 +2,13 @@ package com.company.metaverse;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @OpenAPIDefinition(
+        servers = {@Server(url = "/", description = "Default Server URL")},
         info = @Info(
                 title = "Metaverse",
                 description = "Everything is possible",
